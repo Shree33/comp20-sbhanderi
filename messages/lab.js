e2dfbe18b78be2baf06a6f00212db36ea57f9b0e
shree33 <skbhanderi3@gmail.com>
2016-03-06 20:44:23 -0500

    Finish json script, begin css file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,21 @@
-// Your JavaScript goes here...
+
+function parse() {
+    var request = new XMLHttpRequest()
+    elem = document.getElementById("messages");
+    request.open('GET', 'data.json', true);
+
+    request.onreadystatechange = function () {
+
+        if (request.readyState == 4 && request.status == 200) {
+            data = request.responseText;
+            parsed = JSON.parse(data);
+
+            for (count = 0; count < parsed.length; count++) {
+                elem.innerHTML += "<p>" +  parsed[count]["content"] + " " + parsed[count]["username"] + "</p>";
+            }
+        }
+    }
+
+    request.send(null)
+
+}
