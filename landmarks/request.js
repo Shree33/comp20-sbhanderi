7a2c63bbee66095d3e4f1cbc381d64bc6be921f7
shree33 <skbhanderi3@gmail.com>
2016-03-13 17:17:01 -0400

    Write function to send location to server

--- landmarks/request.js
+++ landmarks/request.js
@@ -1,9 +1,11 @@
 
 function parse() {
     var request = new XMLHttpRequest()
+    var url = 'https://defense-in-derpth.herokuapp.com/sendLocation'
+    var params = "login=LINDA_BRITT&lat=myLat&lng=myLong"
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
-    elem = document.getElementById("map_canvas");
-    request.open('POST', 'https://defense-in-derpth.herokuapp.com/sendLocation', true);
+    elem = document.getElementById("");
+    request.open('POST', url , true);
 
     request.onreadystatechange = function () {
     }
